0d97c9ea73099379d628591bf96cead2627e6598
Iktear Uddin <emon5aurk@gmail.com>
2020-10-31 19:53:21 +0600

    added getUser route for admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,14 +5,15 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddlware.js";
+import { protect, admin } from "../middleware/authMiddlware.js";
 
 router.post("/login", authUser);
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect,admin, getUsers);
 
 export default router;
