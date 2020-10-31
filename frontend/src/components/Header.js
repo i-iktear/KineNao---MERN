438a3d629bc44d11c72fe267e95a0c86a7094511
Iktear Uddin <emon5aurk@gmail.com>
2020-10-31 19:55:10 +0600

    added admin options in Navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -30,14 +30,15 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
 
-
               {userInfo ? (
                 <NavDropdown title={userInfo.name} id={"username"}>
                   <LinkContainer to="/profile">
                     <NavDropdown.Item>My Profile</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/order">
-                    <NavDropdown.Item eventKey="4.1">My Orders</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="4.1">
+                      My Orders
+                    </NavDropdown.Item>
                   </LinkContainer>
                   <NavDropdown.Divider />
                   <NavDropdown.Item onClick={logoutHandler}>
@@ -50,6 +51,19 @@ const Header = () => {
                     <i className="fas fa-user"></i>Login
                   </Nav.Link>
                 </LinkContainer>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
               )}
             </Nav>
           </Navbar.Collapse>
