c0d6d18332b1a4032921b909469f3c8302936b76
Iktear Uddin <emon5aurk@gmail.com>
2020-11-01 18:06:27 +0600

    Added: proteced getUser,updateuser route for Admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddlware.js";
 
@@ -16,6 +18,10 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 router.route("/").post(registerUser).get(protect, admin, getUsers);
-router.route("/:id").delete(protect, admin, deleteUser);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
