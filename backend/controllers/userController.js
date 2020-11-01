807d441d03787b71aa4fd60a2f25f450035b74cd
Iktear Uddin <emon5aurk@gmail.com>
2020-11-01 18:05:21 +0600

    Added: getUserById & updateUser for Admin only. ❌

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,6 +123,42 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc GET  user by ID
+// @route GET /api/users/:id
+// @access private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+// @desc update user profile
+// @route PUT /api/users/:id
+// @access private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -130,4 +166,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
