2db605d1979e85ecec3be05f44772b411ec3a9c6
Iktear Uddin <emon5aurk@gmail.com>
2020-11-02 14:54:00 +0600

    Added: protected  deleteProduct route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { protect, admin } from "../middleware/authMiddlware.js";
 const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
 
 router.route("/").get(getProducts);
 
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
