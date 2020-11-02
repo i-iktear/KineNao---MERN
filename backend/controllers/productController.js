da5f20bd3b34119ddc4e86936d8368783b1cf835
Iktear Uddin <emon5aurk@gmail.com>
2020-11-02 14:53:30 +0600

    Added: deleteProduct controller for Admin only

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -9,7 +9,6 @@ const getProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-
 // @desc fetch single product
 // @route GET /api/products/:id
 // @access Public
@@ -22,5 +21,17 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Delete a product
+// @route DELETE /api/products/:id
+// @access private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "product Removed" });
+  } else {
+    throw new Error("product not Found");
+  }
+});
 
-export { getProducts, getProductById }
+export { getProducts, getProductById, deleteProduct };
