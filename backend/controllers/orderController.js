d73b6983b3d68e14d1def3f1b90a95904f5a118e
Iktear Uddin <emon5aurk@gmail.com>
2020-11-04 22:45:50 +0600

    Added: updateOrderToDelivered controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -91,10 +91,29 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
+// @desc update order to delivered
+// @route GET /api/orders/:id/deliver
+// @access private/ Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order Not Found");
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 };
