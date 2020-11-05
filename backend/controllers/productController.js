5540d0963d3e66785d6786b0fb2b841dcec932a8
Iktear Uddin <emon5aurk@gmail.com>
2020-11-05 12:06:31 +0600

    Added: createProductReview controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -85,10 +85,57 @@ const updateProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not Found");
   }
 });
+
+// @desc Create new review
+// @route POST /api/products/:id/reviews
+// @access private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(404);
+      throw new Error("Product Already Reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ message: "review Added" });
+
+    const updatedProduct = await product.save();
+
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
